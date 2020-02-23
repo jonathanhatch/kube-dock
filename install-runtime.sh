22e66f28e13ded7da30fa64ed233b4ff4aa44522
Jonathan Hatch <38332640+jonathanhatch@users.noreply.github.com>
2020-02-23 10:10:34 -0600

    Update install-runtime.sh

--- install-runtime.sh
+++ install-runtime.sh
@@ -22,6 +22,11 @@ cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 mkdir -p /etc/systemd/system/docker.service.d
+sudo apt-get install -y iptables arptables ebtables
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
+sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 sysctl net.bridge.bridge-nf-call-iptables=1
 systemctl daemon-reload
 systemctl restart docker
