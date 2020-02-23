2a48abdb3ee313231793698b36baf5b08a4fcd47
Jonathan Hatch <38332640+jonathanhatch@users.noreply.github.com>
2020-02-23 10:11:51 -0600

    Update install-runtime.sh

--- install-runtime.sh
+++ install-runtime.sh
@@ -30,3 +30,11 @@ sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 sysctl net.bridge.bridge-nf-call-iptables=1
 systemctl daemon-reload
 systemctl restart docker
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
